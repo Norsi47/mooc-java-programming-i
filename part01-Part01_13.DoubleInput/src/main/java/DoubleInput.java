b182c735630a0f43a7d820d90a96aeadfd50c50e
Norsi47 <briggsnorsi@yahoo.com>
2025-07-01 17:02:31 -0400

    double input class

--- part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
+++ part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
@@ -7,6 +7,13 @@ public class DoubleInput {
         Scanner scanner = new Scanner(System.in);
 
         // write your program here
+        //ask user
+        System.out.println("Give a number: ");
+        double floatingNum = Double.valueOf(scanner.nextLine());
+
+        //print number
+        System.out.println("You gave the number " +floatingNum);
+
 
     }
 }
