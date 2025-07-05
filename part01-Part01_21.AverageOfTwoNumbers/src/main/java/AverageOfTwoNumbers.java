05dbfa6bc43abdc5c4086b8274a18396aed4864b
Norsi47 <briggsnorsi@yahoo.com>
2025-07-04 22:36:54 -0400

    learning average

--- part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
+++ part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
@@ -7,6 +7,12 @@ public class AverageOfTwoNumbers {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give the first number: ");
+        Integer firstNum = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number: ");
+        Integer secondNum = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("The average is " + (double) (firstNum + secondNum) / 2 );
 
     }
 }
--- part01-Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
+++ part01-Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
@@ -7,6 +7,18 @@ public class AverageOfThreeNumbers {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give the first number: ");
+        Integer firstNum = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number: ");
+        Integer secondNum = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the third number: ");
+        Integer thirdNum = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("The average is " + (double)(firstNum + secondNum + thirdNum) / 3 );
+
+
 
     }
 }
