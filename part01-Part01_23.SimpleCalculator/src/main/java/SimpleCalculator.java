c8bf8c791c48bd75c6f1420cb9381a3e6c3b92c2
Norsi47 <briggsnorsi@yahoo.com>
2025-07-05 21:32:57 -0400

    mini calculator

--- part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -7,6 +7,25 @@ public class SimpleCalculator {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give the first number: ");
+        Integer firstNum = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number: ");
+        Integer secondNum = Integer.valueOf(scanner.nextLine());
+
+        //do the math
+        System.out.println(firstNum + " + " + secondNum + " = " + (firstNum + secondNum));
+
+        //subtract
+        System.out.println(firstNum + " - " + secondNum + " = " + (firstNum - secondNum));
+
+        //multiply
+        System.out.println(firstNum + " * " + secondNum + " = " + (firstNum * secondNum));
+
+        //divide
+        System.out.println(firstNum + " / " + secondNum + " = " + (double) firstNum / secondNum);
+
+
 
     }
 }
