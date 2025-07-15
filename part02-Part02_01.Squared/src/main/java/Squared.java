2d1306e375e973fbacab58b958c50466da371fad
Norsi47 <briggsnorsi@yahoo.com>
2025-07-14 21:53:28 -0400

    square root of numbers

--- part02-Part02_01.Squared/src/main/java/Squared.java
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -6,5 +6,12 @@ public class Squared {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Integer num1 = Integer.valueOf(scanner.nextLine());
+
+        int productOff = num1 * num1;
+
+        System.out.println(productOff);
+
+
     }
 }
--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -6,5 +6,15 @@ public class SquareRootOfSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Integer num1 = Integer.valueOf(scanner.nextLine());
+
+        Integer num2 = Integer.valueOf(scanner.nextLine());
+
+        int sumNum = num1 + num2;
+
+        double squareRootOfSum = Math.sqrt(sumNum);
+        
+        System.out.println(squareRootOfSum);
+
     }
 }
