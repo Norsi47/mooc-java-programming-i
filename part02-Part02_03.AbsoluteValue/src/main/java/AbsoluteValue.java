168864ce122cc9eed09370cf88b98a0c310eb843
Norsi47 <briggsnorsi@yahoo.com>
2025-07-15 22:53:56 -0400

    more on if and else statements

--- part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -6,5 +6,18 @@ public class AbsoluteValue {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int userNum = Integer.valueOf(scanner.nextLine());
+
+        int multiNum = userNum * -1;
+
+        if (userNum < 0) {
+            System.out.println(multiNum);
+            
+        } else {
+            System.out.println(userNum);
+        }
+
+       
+
     }
 }
