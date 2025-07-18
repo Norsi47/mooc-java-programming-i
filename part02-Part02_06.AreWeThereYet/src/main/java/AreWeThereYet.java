6e8373f467a2865fe42b71a031fcc736bc93f51a
Norsi47 <briggsnorsi@yahoo.com>
2025-07-18 17:00:53 -0400

    starting on loops

--- part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
+++ part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
@@ -5,6 +5,15 @@ public class AreWeThereYet {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Give a number: ");
+            int userNum = Integer.valueOf(scanner.nextLine());
+            if (userNum == 4) {
+                break;
+                
+            }
+            
+        }
 
     }
 }
