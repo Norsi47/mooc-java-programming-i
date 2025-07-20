8f468d457abeab93f403af80d1e1ceef631e7b44
Norsi47 <briggsnorsi@yahoo.com>
2025-07-19 21:54:24 -0400

    MORE LOOPS THINGS

--- part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -6,5 +6,28 @@ public class OnlyPositives {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Give a number: ");
+            int userNum = Integer.valueOf(scanner.nextLine());
+
+            if (userNum < 0) {
+                System.out.println("Unsuitable number");
+
+                
+            }
+
+            if (userNum > 0) {
+                System.out.println(userNum * userNum);
+                
+            }            
+            if (userNum == 0) {
+                break;
+                
+            }
+
+
+            
+        }
+
     }
 }
