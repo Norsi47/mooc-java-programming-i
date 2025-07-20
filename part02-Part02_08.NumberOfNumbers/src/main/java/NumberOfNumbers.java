2e7d65eeb1398221fc99a7aae9245260e18659e2
Norsi47 <briggsnorsi@yahoo.com>
2025-07-20 14:47:34 -0400

    using loops with showing how many

--- part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -6,5 +6,25 @@ public class NumberOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int ones = 0;
+
+        while (true) {
+            System.out.println("Give a number: ");
+            int userNum = Integer.valueOf(scanner.nextLine());
+
+            if (userNum == 0) {
+                break;
+
+            } else if (userNum > 0) {
+                ones = ones + 1;
+
+            } else if (userNum < 0) {
+                ones = ones + 1;
+                
+            }
+
+        }
+        System.out.println("Number of numbers: " + ones);
+
     }
 }
