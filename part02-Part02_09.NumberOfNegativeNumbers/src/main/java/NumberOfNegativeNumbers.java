cb32779a5949053798fa7331e114a4b00d46cbac
Norsi47 <briggsnorsi@yahoo.com>
2025-07-20 17:22:53 -0400

    use while loops and add the sum

--- part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
+++ part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -5,6 +5,26 @@ public class NumberOfNegativeNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int countNum = 0;
+
+        while (true) {
+            System.out.println("Give a number: ");
+            int userNum = Integer.valueOf(scanner.nextLine());
+
+            if (userNum == 0) {
+                break;
+
+            }
+            if (userNum > 0) {
+                continue;
+
+            } else if (userNum < 0) {
+                countNum = countNum + 1;
+
+            }
+
+        }
+        System.out.println("Number of negative numbers: " + countNum);
 
     }
 }
--- part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -6,5 +6,20 @@ public class SumOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sumOfNumbers = 0;
+
+        while (true) {
+            System.out.println("Give a number: ");
+            int userNum = Integer.valueOf(scanner.nextLine());
+
+            if (userNum == 0) {
+                break;
+
+            }
+            sumOfNumbers = sumOfNumbers + userNum;
+
+        }
+        System.out.println("Sum of the numbers: " + sumOfNumbers);
+
     }
 }
