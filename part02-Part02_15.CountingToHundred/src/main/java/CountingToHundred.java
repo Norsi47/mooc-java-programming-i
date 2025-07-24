a10e9eb47dda0714176b346e799ba7a0a9927939
Norsi47 <briggsnorsi@yahoo.com>
2025-07-23 21:31:00 -0400

    for loop and using if statements

--- part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -6,5 +6,12 @@ public class CountingToHundred {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int userNum = Integer.valueOf(scanner.nextLine());
+
+        for (int i = userNum; i < 101; i++) {
+            System.out.println(i);
+            
+        }
+
     }
 }
--- part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -7,5 +7,31 @@ public class FromWhereToWhere {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.print("Where to? ");
+        int userNum = Integer.valueOf(scanner.nextLine());
+
+        System.out.print("Where from? ");
+        int userNum2 = Integer.valueOf(scanner.nextLine());
+
+        if (userNum2 <= userNum) {
+            for (int i = userNum2; i <= userNum; i ++) {
+                System.out.println(i);
+
+            }
+
+
+            
+        }
+
+
+
+
+
+        
+
+
+
+
+
     }
 }
