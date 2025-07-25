a8c62b98ad0f170e0c73e6155864c1655eb6270e
Norsi47 <briggsnorsi@yahoo.com>
2025-07-25 16:18:50 -0400

    using factorial

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -6,5 +6,19 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
+        int factorialNum = 1;
+        System.out.print("Give a number: ");
+        int userNum = Integer.valueOf(scanner.nextLine());
+
+        for(int i = factorialNum; i<=userNum; i++) {
+            factorialNum = factorialNum * i;
+
+        }
+
+        System.out.print("Factorial: " +factorialNum);
+
+
+
     }
 }
