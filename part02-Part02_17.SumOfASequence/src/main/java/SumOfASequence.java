547f5aba54a740649ab9130591035b24ef7b77de
Norsi47 <briggsnorsi@yahoo.com>
2025-07-24 21:32:33 -0400

    more loops

--- part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -6,5 +6,17 @@ public class SumOfASequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Last number? ");
+        int userNum = Integer.valueOf(scanner.nextLine());
+
+        int sumOfNum = 0;
+
+        for (int i = 1; i <= userNum; i++) {
+
+            sumOfNum = sumOfNum + i;
+
+        }
+        System.out.println("The sum is " + sumOfNum);
+
     }
 }
--- part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -6,5 +6,24 @@ public class SumOfASequenceTheSequel {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("First number? ");
+        int firstNum = Integer.valueOf(scanner.nextLine());
+
+        System.out.print("Last number? ");
+        int secondNum = Integer.valueOf(scanner.nextLine());
+
+        int sumOfNum = 0;
+
+        if (firstNum < secondNum) {
+            for (int i = firstNum; i <= secondNum; i++) {
+                sumOfNum = sumOfNum + i;
+
+            }
+            
+        }
+        System.out.print("The sum is: " +sumOfNum);
+
+
+
     }
 }
