fd23127a9cb3ae09bb64b429e02f9e89a4949bc5
Norsi47 <briggsnorsi@yahoo.com>
2025-07-31 16:07:34 -0400

    multiple meothods with loops

--- part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
+++ part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
@@ -3,7 +3,19 @@
 public class FromParameterToOne {
 
     public static void main(String[] args) {
+        printFromNumberToOne(5);
+        printFromNumberToOne(2);
 
+    }
+
+    public static void printFromNumberToOne(int number) {
+        //start from numbers
+        int i = number;
+        while (i >= 1) {
+            System.out.println(i);
+            i--;
+            
+        }
 
     }
 
