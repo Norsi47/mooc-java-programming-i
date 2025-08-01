1f9ed1523c7c7096b219bb52b2832849ff96c31a
Norsi47 <briggsnorsi@yahoo.com>
2025-08-01 16:35:00 -0400

    different moethds and using for loops

--- part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -4,7 +4,22 @@ import java.util.Scanner;
 public class DivisibleByThree {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
+        divisibleByThreeInRange(2, 10);
+
+    }
+
+    public static void divisibleByThreeInRange (int beginning, int end) {
+        //loop to keep track of # and print multi numbers
+        for (int i = beginning; i<=end; i++) {
+            // if staement for moduler and == 0 for no remainder
+            if (i % 3 == 0 ) {
+                //print out the # divisibl by three
+                System.out.println(i);
+                
+            }
+
+        }
 
     }
 
